a5296970a1e477c8b0b27a874c33bf930117a1f4
Eash Aggarwal <eash268@gmail.com>
2024-06-02 19:42:36 -0400

    save

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "@clerk/clerk-expo";
 
 export const HomeScreen = () => {
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+
+  // when user is loaded, print get token
+  if (isLoaded) {
+    getToken().then((token) => console.log(token));
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>HomeScreen</Text>
