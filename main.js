cae5c0ccd3b995b16ba04011636c66ffe5192e7f
Eash Aggarwal <eash268@gmail.com>
2024-06-03 22:02:40 -0400

    save

--- main.js
+++ main.js
@@ -1,41 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { HomeStack } from "./src/navigation/HomeStack";
-import { SearchStack } from "./src/navigation/SearchStack";
-import { ProfileStack } from "./src/navigation/ProfileStack";
+import { HomeTabs } from "./src/navigation/HomeTabs";
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export const Main = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let icon;
-
-            if (route.name === "Home") {
-              icon = focused ? "home" : "home";
-            } else if (route.name === "Search") {
-              icon = focused ? "search1" : "search1";
-            } else if (route.name === "Profile") {
-              icon = focused ? "user" : "user";
-            }
-
-            return <AntDesign name={icon} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "blue",
-          tabBarInactiveTintColor: "gray",
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Search" component={SearchStack} />
-        <Tab.Screen name="Profile" component={ProfileStack} />
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeTabs} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
